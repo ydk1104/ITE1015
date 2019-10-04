4ebc2f83b5d8aff81851886622e98ff722621687
2019065687 <ydk1104@hanyang.ac.kr>
2019-10-04 13:46:49 +0900

    hw5-1

--- hw5-1/cmdarg.cc
+++ hw5-1/cmdarg.cc
@@ -5,7 +5,7 @@
 int main(int argc, char** argv){
 	std::string s;
 	int sum = 0;
-	for(int i=0; i<argc; i++){
+	for(int i=1; i<argc; i++){
 		if(atoi(argv[i])) sum += atoi(argv[i]);
 		else s += argv[i];
 	}
